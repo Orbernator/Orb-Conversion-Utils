8156616345e7ba6c28b0a96b8167ca009e53d49c
Orbernator <E_pidog@protonmail.com>
2025-12-10 07:56:55 +1030

    Added the basic Conversion system

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import os
 from art import tprint
 
+filestep1 = 0
+filestep2 = 0
+step1 = 0
+step2 = 0
+
+def conversion(convert1, convert2, inputFile, outputFile):
+    if convert1 == (1) and convert2 == (2):
+        os.system('ffmpeg -i ' + inputFile + ' -vn -acodec libmp3lame -b:a 192k ' + outputFile)
+    else:
+        print('Somthing Went Wrong')
+
 def filenameMenu(step):
     if step == (1):
         filestep1 = input('Enter the input file name>> ')
@@ -26,4 +37,7 @@ print('Conversion Utilities')
 print('--------------------\nOrb Conversion Utils is licenced under the GNU General Public License 3.0')
 convertMenu(1)
 convertMenu(2)
+filenameMenu(1)
+filenameMenu(2)
+conversion(step1,step2,filestep1,filestep2)
 
